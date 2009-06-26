965509cc9c179a20b6325428f4b3ad869537152c
Andre Masella <andre@kuum.wlu.ca>
2009-06-26 18:37:51 -0400

    Added missing generic

--- src/ca/wlu/gisql/cytoscape/ToCyNetwork.java
+++ src/ca/wlu/gisql/cytoscape/ToCyNetwork.java
@@ -9,6 +9,7 @@ import ca.wlu.gisql.environment.Environment;
 import ca.wlu.gisql.environment.parser.Literal;
 import ca.wlu.gisql.environment.parser.Parseable;
 import ca.wlu.gisql.environment.parser.Parser;
+import ca.wlu.gisql.environment.parser.ParserKnowledgebase;
 import ca.wlu.gisql.environment.parser.Token;
 import ca.wlu.gisql.environment.parser.ast.AstNode;
 import ca.wlu.gisql.graph.Gene;
@@ -76,7 +77,7 @@ public class ToCyNetwork implements Interactome {
 			return false;
 		}
 
-		public void show(ShowablePrintWriter print) {
+		public void show(ShowablePrintWriter<ParserKnowledgebase> print) {
 			print.print("Create Cytoscape Network: A @ *");
 		}
 
